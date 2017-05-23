71bc24e9e80cc97d343a630f347029b85c88f2e2
Xudong Bai <sheldon.bai1989@gmail.com>
2017-05-22 21:17:09 -0700

    add brackets.c

---
+++ 61.brackets/brackets.c
@@ -0,0 +1,100 @@
+#include <unistd.h>
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		++i;
+	return (i);
+}
+
+int		ft_iskuohao(char c)
+{
+	if (c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}')
+		return (1);
+	else
+		return (0);
+}
+
+int		ft_brackets(char *str)
+{
+	int		len;
+	int		i;
+	int		loop1;
+	int		loop2;
+	int		loop3;
+	char	curchar;
+	int		j;
+
+	len = ft_strlen(str);
+	if (len < 1)
+		return (1);
+	if (len == 1 && ft_iskuohao(str[0]))
+		return (0);
+	i = -1;
+	while (++i < len)
+	{
+		curchar = str[i];
+		loop1 = 0;
+		loop2 = 0;
+		loop3 = 0;
+		if (curchar == ')')
+			loop1++;
+		else if (curchar == ']')
+			loop2++;
+		else if (curchar == '}')
+			loop3++;
+		if (loop1 || loop2 || loop3)
+			str[i] = 'd';
+		j = i;
+		while (loop1 > 0)
+		{
+			if (--j < 0)
+				return (0);
+			if (str[j] == '(')
+				loop1--;
+			str[j] = 'a';
+		}
+		while (loop2 > 0)
+		{
+			if (--j < 0)
+				return (0);
+			if (str[j] == '[')
+				loop2--;
+			str[j] = 'b';
+		}
+		while (loop3 > 0)
+		{
+			if (--j < 0)
+				return (0);
+			if (str[j] == '{')
+				loop3--;
+			str[j] = 'c';
+		}
+	}
+	return (1);
+}
+
+int		main(int argc, char **argv)
+{
+	int		i;
+	int		ret;
+
+	if (argc > 1)
+	{
+		i = 0;
+		while (++i < argc)
+		{
+			ret = ft_brackets(argv[i]);
+			if (ret)
+				write(1, "OK\n", 3);
+			else
+				write(1, "Error\n", 6);
+		}
+	}
+	else
+		write(1, "\n", 1);
+	return (0);
+}
